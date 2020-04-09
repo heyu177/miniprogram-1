1e35018ac275dda2dd919d20037e9a2100a2d4da
heyu <heyu@qq.com>
2020-04-09 22:32:24 +0800

    2020.4.9 22:32已提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,8 @@ const app = getApp()
 
 Page({
   data: {
-    msg:"北方汉子"
+    msg:"北方汉子",
+    userInfo:{}
   },
   //事件处理函数
   bindViewTap: function() {
@@ -12,33 +13,15 @@ Page({
       url: '../logs/logs'
     })
   },
+  // 页面加载时执行
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
+    wx.getUserInfo({
+      success:(data) => {
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+          userInfo:data.userInfo
+        });
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    })
   },
   getUserInfo: function(e) {
     console.log(e)
